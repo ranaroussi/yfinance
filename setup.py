ed8a8c31e03e4a5385e226f8e98853330e6ceb9d
ran <ran@aroussi.com>
2019-04-29 18:14:43 +0300

    removed support for python 3.4

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
 
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
