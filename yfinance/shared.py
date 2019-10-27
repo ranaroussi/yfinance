ad45b731010ac9d5289032341e7781ffab6c4652
ran <ran@aroussi.com>
2019-10-27 19:21:35 +0200

    introduced new fundamentals + refactoring

--- yfinance/shared.py
+++ yfinance/shared.py
@@ -19,26 +19,6 @@
 # limitations under the License.
 #
 
-__version__ = "0.1.46"
-__author__ = "Ran Aroussi"
-
-from .ticker import Ticker
-from .tickers import Tickers
-from .multi import download
-
-
-def pdr_override():
-    """
-    make pandas datareader optional
-    otherwise can be called via fix_yahoo_finance.download(...)
-    """
-    try:
-        import pandas_datareader
-        pandas_datareader.data.get_data_yahoo = download
-        pandas_datareader.data.get_data_yahoo_actions = download
-        pandas_datareader.data.DataReader = download
-    except Exception:
-        pass
-
-
-__all__ = ['download', 'Ticker', 'Tickers', 'pdr_override']
+_DFS = {}
+_PROGRESS_BAR = None
+_ERRORS = {}
