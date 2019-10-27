18879c5b3121e034d33a98fdcf816cfe13034ba6
ran <ran@aroussi.com>
2019-10-27 19:21:35 +0200

    introduced new fundamentals + refactoring

---
+++ yfinance/base.py
@@ -0,0 +1,415 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Yahoo! Finance market data downloader (+fix for Pandas Datareader)
+# https://github.com/ranaroussi/yfinance
+#
+# Copyright 2017-2019 Ran Aroussi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import print_function
+
+import time as _time
+import datetime as _datetime
+import requests as _requests
+import pandas as _pd
+import numpy as _np
+
+from . import utils
+
+# import json as _json
+# import re as _re
+# import sys as _sys
+
+from . import shared
+
+
+class TickerBase():
+    def __init__(self, ticker):
+        self.ticker = ticker.upper()
+        self._history = None
+        self._base_url = 'https://query1.finance.yahoo.com'
+        self._scrape_url = 'https://finance.yahoo.com/quote'
+
+        self._fundamentals = False
+        self._info = None
+        self._sustainability = None
+        self._recommendations = None
+        self._calendar = None
+
+        self._earnings = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+        self._financials = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+        self._balancesheet = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+        self._cashflow = {
+            "yearly": utils.empty_df(),
+            "quarterly": utils.empty_df()}
+
+    def history(self, period="1mo", interval="1d",
+                start=None, end=None, prepost=False, actions=True,
+                auto_adjust=True, back_adjust=False,
+                proxy=None, rounding=True, tz=None, **kwargs):
+        """
+        :Parameters:
+            period : str
+                Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+                Either Use period parameter or use start and end
+            interval : str
+                Valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+                Intraday data cannot extend last 60 days
+            start: str
+                Download start date string (YYYY-MM-DD) or _datetime.
+                Default is 1900-01-01
+            end: str
+                Download end date string (YYYY-MM-DD) or _datetime.
+                Default is now
+            prepost : bool
+                Include Pre and Post market data in results?
+                Default is False
+            auto_adjust: bool
+                Adjust all OHLC automatically? Default is True
+            back_adjust: bool
+                Back-adjusted data to mimic true historical prices
+            proxy: str
+                Optional. Proxy server URL scheme. Default is None
+            rounding: bool
+                Round values to 2 decimal places?
+                Optional. Default is False = precision suggested by Yahoo!
+            tz: str
+                Optional timezone locale for dates.
+                (default data is returned as non-localized dates)
+        """
+
+        if start or period is None or period.lower() == "max":
+            if start is None:
+                start = -2208988800
+            elif isinstance(start, _datetime.datetime):
+                start = int(_time.mktime(start.timetuple()))
+            else:
+                start = int(_time.mktime(
+                    _time.strptime(str(start), '%Y-%m-%d')))
+            if end is None:
+                end = int(_time.time())
+            elif isinstance(end, _datetime.datetime):
+                end = int(_time.mktime(end.timetuple()))
+            else:
+                end = int(_time.mktime(_time.strptime(str(end), '%Y-%m-%d')))
+
+            params = {"period1": start, "period2": end}
+        else:
+            period = period.lower()
+            params = {"range": period}
+
+        params["interval"] = interval.lower()
+        params["includePrePost"] = prepost
+        params["events"] = "div,splits"
+
+        # 1) fix weired bug with Yahoo! - returning 60m for 30m bars
+        if params["interval"] == "30m":
+            params["interval"] = "15m"
+
+        # setup proxy in requests format
+        if proxy is not None:
+            if isinstance(proxy, dict) and "https" in proxy:
+                proxy = proxy["https"]
+            proxy = {"https": proxy}
+
+        # Getting data from json
+        url = "{}/v8/finance/chart/{}".format(self._base_url, self.ticker)
+        data = _requests.get(url=url, params=params, proxies=proxy)
+        if "Will be right back" in data.text:
+            raise RuntimeError("*** YAHOO! FINANCE IS CURRENTLY DOWN! ***\n"
+                               "Our engineers are working quickly to resolve "
+                               "the issue. Thank you for your patience.")
+        data = data.json()
+
+        # Work with errors
+        err_msg = "No data found for this date range, symbol may be delisted"
+        if "chart" in data and data["chart"]["error"]:
+            err_msg = data["chart"]["error"]["description"]
+            shared._DFS[self.ticker] = utils.empty_df()
+            shared._ERRORS[self.ticker] = err_msg
+            if "many" not in kwargs:
+                print('- %s: %s' % (self.ticker, err_msg))
+            return shared._DFS[self.ticker]
+
+        elif "chart" not in data or data["chart"]["result"] is None or \
+                not data["chart"]["result"]:
+            shared._DFS[self.ticker] = utils.empty_df()
+            shared._ERRORS[self.ticker] = err_msg
+            if "many" not in kwargs:
+                print('- %s: %s' % (self.ticker, err_msg))
+            return shared._DFS[self.ticker]
+
+        # parse quotes
+        try:
+            quotes = utils.parse_quotes(data["chart"]["result"][0], tz)
+        except Exception:
+            shared._DFS[self.ticker] = utils.empty_df()
+            shared._ERRORS[self.ticker] = err_msg
+            if "many" not in kwargs:
+                print('- %s: %s' % (self.ticker, err_msg))
+            return shared._DFS[self.ticker]
+
+        # 2) fix weired bug with Yahoo! - returning 60m for 30m bars
+        if interval.lower() == "30m":
+            quotes2 = quotes.resample('30T')
+            quotes = _pd.DataFrame(index=quotes2.last().index, data={
+                'Open': quotes2['Open'].first(),
+                'High': quotes2['High'].max(),
+                'Low': quotes2['Low'].min(),
+                'Close': quotes2['Close'].last(),
+                'Adj Close': quotes2['Adj Close'].last(),
+                'Volume': quotes2['Volume'].sum()
+            })
+            try:
+                quotes['Dividends'] = quotes2['Dividends'].max()
+            except Exception:
+                pass
+            try:
+                quotes['Stock Splits'] = quotes2['Dividends'].max()
+            except Exception:
+                pass
+
+        if auto_adjust:
+            quotes = utils.auto_adjust(quotes)
+        elif back_adjust:
+            quotes = utils.back_adjust(quotes)
+
+        if rounding:
+            quotes = _np.round(quotes, data[
+                "chart"]["result"][0]["meta"]["priceHint"])
+        quotes['Volume'] = quotes['Volume'].fillna(0).astype(_np.int64)
+
+        quotes.dropna(inplace=True)
+
+        # actions
+        dividends, splits = utils.parse_actions(data["chart"]["result"][0], tz)
+
+        # combine
+        df = _pd.concat([quotes, dividends, splits], axis=1, sort=True)
+        df["Dividends"].fillna(0, inplace=True)
+        df["Stock Splits"].fillna(0, inplace=True)
+
+        # index eod/intraday
+        df.index = df.index.tz_localize("UTC").tz_convert(
+            data["chart"]["result"][0]["meta"]["exchangeTimezoneName"])
+
+        if params["interval"][-1] == "m":
+            df.index.name = "Datetime"
+        else:
+            df.index = _pd.to_datetime(df.index.date)
+            if tz is not None:
+                df.index = df.index.tz_localize(tz)
+            df.index.name = "Date"
+
+        self._history = df.copy()
+
+        if not actions:
+            df.drop(columns=["Dividends", "Stock Splits"], inplace=True)
+
+        return df
+
+    # ------------------------
+
+    def _get_fundamentals(self, kind=None, proxy=None):
+        def cleanup(data):
+            df = _pd.DataFrame(data).drop(columns=['maxAge'])
+            for col in df.columns:
+                df[col] = _np.where(df[col] == '-', _np.nan, df[col])
+
+            df.set_index('endDate', inplace=True)
+            df.index = _pd.to_datetime(df.index, unit='s')
+            df = df.T
+            df.columns.name = ''
+            df.index.name = 'Breakdown'
+
+            df.index = utils.camel2title(df.index)
+            return df
+
+        # setup proxy in requests format
+        if proxy is not None:
+            if isinstance(proxy, dict) and "https" in proxy:
+                proxy = proxy["https"]
+            proxy = {"https": proxy}
+
+        if self._fundamentals:
+            return
+
+        # get info and sustainability
+        url = '%s/%s' % (self._scrape_url, self.ticker)
+        data = utils.get_json(url, proxy)
+
+        # sustainability
+        d = {}
+        for item in data['esgScores']:
+            if not isinstance(data['esgScores'][item], dict):
+                d[item] = data['esgScores'][item]
+
+        s = _pd.DataFrame(d)[-1:].T
+        s.columns = ['Value']
+        s.index.name = '%.f-%.f' % (
+            s[s.index == 'ratingYear']['Value'].values[0],
+            s[s.index == 'ratingMonth']['Value'].values[0])
+
+        self._sustainability = s[~s.index.isin(
+            ['maxAge', 'ratingYear', 'ratingMonth'])]
+
+        # info
+        self._info = {
+            **data['summaryProfile'],
+            **data['summaryProfile'],
+            **data['defaultKeyStatistics']
+        }
+
+        # events
+        try:
+            cal = _pd.DataFrame(
+                data['calendarEvents']['earnings'])
+            cal['earningsDate'] = _pd.to_datetime(
+                cal['earningsDate'], unit='s')
+            self._calendar = cal.T
+            self._calendar.index = utils.camel2title(self._calendar.index)
+            self._calendar.columns = ['Value']
+        except Exception:
+            pass
+
+        # analyst recommendations
+        try:
+            rec = _pd.DataFrame(
+                data['upgradeDowngradeHistory']['history'])
+            rec['earningsDate'] = _pd.to_datetime(
+                rec['epochGradeDate'], unit='s')
+            rec.set_index('earningsDate', inplace=True)
+            rec.index.name = 'Date'
+            rec.columns = utils.camel2title(rec.columns)
+            self._recommendations = rec[[
+                'Firm', 'To Grade', 'From Grade', 'Action']].sort_index()
+        except Exception:
+            pass
+
+        # get fundamentals
+        data = utils.get_json(url+'/financials', proxy)
+
+        # generic patterns
+        for key in (
+            (self._cashflow, 'cashflowStatement', 'cashflowStatements'),
+            (self._balancesheet, 'balanceSheet', 'balanceSheetStatements'),
+            (self._financials, 'incomeStatement', 'incomeStatementHistory')
+        ):
+
+            item = key[1] + 'History'
+            if item in data:
+                key[0]['yearly'] = cleanup(data[item][key[2]])
+
+            item = key[1]+'HistoryQuarterly'
+            if item in data:
+                key[0]['quarterly'] = cleanup(data[item][key[2]])
+
+        # earnings
+        earnings = data['earnings']['financialsChart']
+        df = _pd.DataFrame(earnings['yearly']).set_index('date')
+        df.columns = utils.camel2title(df.columns)
+        df.index.name = 'Year'
+        self._earnings['yearly'] = df
+
+        df = _pd.DataFrame(earnings['quarterly']).set_index('date')
+        df.columns = utils.camel2title(df.columns)
+        df.index.name = 'Quarter'
+        self._earnings['quarterly'] = df
+
+        self._fundamentals = True
+
+    def get_recommendations(self, proxy=None, as_dict=False, *args, **kwargs):
+        self._get_fundamentals(proxy)
+        data = self._recommendations
+        if as_dict:
+            return data.to_dict()
+        return data
+
+    def get_calendar(self, proxy=None, as_dict=False, *args, **kwargs):
+        self._get_fundamentals(proxy)
+        data = self._calendar
+        if as_dict:
+            return data.to_dict()
+        return data
+
+    def get_info(self, proxy=None, as_dict=False, *args, **kwargs):
+        self._get_fundamentals(proxy)
+        data = self._info
+        if as_dict:
+            return data.to_dict()
+        return data
+
+    def get_sustainability(self, proxy=None, as_dict=False, *args, **kwargs):
+        self._get_fundamentals(proxy)
+        data = self._sustainability
+        if as_dict:
+            return data.to_dict()
+        return data
+
+    def get_earnings(self, proxy=None, as_dict=False, freq="yearly"):
+        self._get_fundamentals(proxy)
+        data = self._earnings[freq]
+        if as_dict:
+            return data.to_dict()
+        return data
+
+    def get_financials(self, proxy=None, as_dict=False, freq="yearly"):
+        self._get_fundamentals(proxy)
+        data = self._financials[freq]
+        if as_dict:
+            return data.to_dict()
+        return data
+
+    def get_balancesheet(self, proxy=None, as_dict=False, freq="yearly"):
+        self._get_fundamentals(proxy)
+        data = self._balancesheet[freq]
+        if as_dict:
+            return data.to_dict()
+        return data
+
+    def get_balance_sheet(self, proxy=None, as_dict=False, freq="yearly"):
+        return self.get_balancesheet(proxy, as_dict, freq)
+
+    def get_cashflow(self, proxy=None, as_dict=False, freq="yearly"):
+        self._get_fundamentals(proxy)
+        data = self._cashflow[freq]
+        if as_dict:
+            return data.to_dict()
+        return data
+
+    def get_dividends(self, proxy=None):
+        if self._history is None:
+            self.history(period="max", proxy=proxy)
+        dividends = self._history["Dividends"]
+        return dividends[dividends != 0]
+
+    def get_splits(self, proxy=None):
+        if self._history is None:
+            self.history(period="max", proxy=proxy)
+        splits = self._history["Stock Splits"]
+        return splits[splits != 0]
+
+    def get_actions(self, proxy=None):
+        if self._history is None:
+            self.history(period="max", proxy=proxy)
+        actions = self._history[["Dividends", "Stock Splits"]]
+        return actions[actions != 0].dropna(how='all').fillna(0)
---
+++ yfinance/multi.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Yahoo! Finance market data downloader (+fix for Pandas Datareader)
+# https://github.com/ranaroussi/yfinance
+#
+# Copyright 2017-2019 Ran Aroussi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import print_function
+
+import time as _time
+import multitasking as _multitasking
+import pandas as _pd
+
+from . import Ticker, utils
+from . import shared
+
+
+def download(tickers, start=None, end=None, actions=False, threads=True,
+             group_by='column', auto_adjust=False, back_adjust=False,
+             progress=True, period="max", interval="1d", prepost=False,
+             proxy=None, rounding=False, **kwargs):
+    """Download yahoo tickers
+    :Parameters:
+        tickers : str, list
+            List of tickers to download
+        period : str
+            Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+            Either Use period parameter or use start and end
+        interval : str
+            Valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+            Intraday data cannot extend last 60 days
+        start: str
+            Download start date string (YYYY-MM-DD) or _datetime.
+            Default is 1900-01-01
+        end: str
+            Download end date string (YYYY-MM-DD) or _datetime.
+            Default is now
+        group_by : str
+            Group by 'ticker' or 'column' (default)
+        prepost : bool
+            Include Pre and Post market data in results?
+            Default is False
+        auto_adjust: bool
+            Adjust all OHLC automatically? Default is False
+        actions: bool
+            Download dividend + stock splits data. Default is False
+        threads: bool / int
+            How many threads to use for mass downloading. Default is True
+        proxy: str
+            Optional. Proxy server URL scheme. Default is None
+        rounding: bool
+            Optional. Round values to 2 decimal places?
+    """
+
+    # create ticker list
+    tickers = tickers if isinstance(
+        tickers, (list, set, tuple)) else tickers.replace(',', ' ').split()
+
+    tickers = list(set([ticker.upper() for ticker in tickers]))
+
+    if progress:
+        shared._PROGRESS_BAR = utils.ProgressBar(len(tickers), 'completed')
+
+    # reset shared._DFS
+    shared._DFS = {}
+    shared._ERRORS = {}
+
+    # download using threads
+    if threads:
+        if threads is True:
+            threads = min([len(tickers), _multitasking.cpu_count() * 2])
+        _multitasking.set_max_threads(threads)
+        for i, ticker in enumerate(tickers):
+            _download_one_threaded(ticker, period=period, interval=interval,
+                                   start=start, end=end, prepost=prepost,
+                                   actions=actions, auto_adjust=auto_adjust,
+                                   back_adjust=back_adjust,
+                                   progress=(progress and i > 0), proxy=proxy,
+                                   rounding=rounding)
+        while len(shared._DFS) < len(tickers):
+            _time.sleep(0.01)
+
+    # download synchronously
+    else:
+        for i, ticker in enumerate(tickers):
+            data = _download_one(ticker, period=period, interval=interval,
+                                 start=start, end=end, prepost=prepost,
+                                 actions=actions, auto_adjust=auto_adjust,
+                                 back_adjust=back_adjust, rounding=rounding)
+            shared._DFS[ticker.upper()] = data
+            if progress:
+                shared._PROGRESS_BAR.animate()
+
+    if progress:
+        shared._PROGRESS_BAR.completed()
+
+    if shared._ERRORS:
+        print('\n%.f Failed download%s:' % (
+            len(shared._ERRORS), 's' if len(shared._ERRORS) > 1 else ''))
+        # print(shared._ERRORS)
+        print("\n".join(['- %s: %s' %
+                         v for v in list(shared._ERRORS.items())]))
+
+    if len(tickers) == 1:
+        return shared._DFS[tickers[0]]
+
+    try:
+        data = _pd.concat(shared._DFS.values(), axis=1,
+                          keys=shared._DFS.keys())
+    except Exception:
+        _realign_dfs()
+        data = _pd.concat(shared._DFS.values(), axis=1,
+                          keys=shared._DFS.keys())
+
+    if group_by == 'column':
+        data.columns = data.columns.swaplevel(0, 1)
+        data.sort_index(level=0, axis=1, inplace=True)
+
+    return data
+
+
+def _realign_dfs():
+    idx_len = 0
+    idx = None
+
+    for df in shared._DFS.values():
+        if len(df) > idx_len:
+            idx_len = len(df)
+            idx = df.index
+
+    for key in shared._DFS.keys():
+        try:
+            shared._DFS[key] = _pd.DataFrame(
+                index=idx, data=shared._DFS[key]).drop_duplicates()
+        except Exception:
+            shared._DFS[key] = _pd.concat([
+                utils.empty_df(idx), shared._DFS[key].dropna()
+            ], axis=0, sort=True)
+
+        # remove duplicate index
+        shared._DFS[key] = shared._DFS[key].loc[
+            ~shared._DFS[key].index.duplicated(keep='last')]
+
+
+@_multitasking.task
+def _download_one_threaded(ticker, start=None, end=None,
+                           auto_adjust=False, back_adjust=False,
+                           actions=False, progress=True, period="max",
+                           interval="1d", prepost=False, proxy=None,
+                           rounding=False):
+
+    data = _download_one(ticker, start, end, auto_adjust, back_adjust,
+                         actions, period, interval, prepost, proxy, rounding)
+    shared._DFS[ticker.upper()] = data
+    if progress:
+        shared._PROGRESS_BAR.animate()
+
+
+def _download_one(ticker, start=None, end=None,
+                  auto_adjust=False, back_adjust=False,
+                  actions=False, period="max", interval="1d",
+                  prepost=False, proxy=None, rounding=False):
+
+    return Ticker(ticker).history(period=period, interval=interval,
+                                  start=start, end=end, prepost=prepost,
+                                  actions=actions, auto_adjust=auto_adjust,
+                                  back_adjust=back_adjust, proxy=proxy,
+                                  rounding=rounding, many=True)
