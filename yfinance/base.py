fb0347ac02478fcb34480d219d6780cd3d65c54c
ran <ran@aroussi.com>
2019-12-27 19:59:57 +0200

    fixed holders always returning results for MSFT

--- yfinance/base.py
+++ yfinance/base.py
@@ -275,13 +275,16 @@ class TickerBase():
         data = utils.get_json(url, proxy)
 
         # holders
-        holders = _pd.read_html('https://finance.yahoo.com/quote/MSFT/holders')
+        url = "{}/{}/holders".format(self._scrape_url, self.ticker)
+        holders = _pd.read_html(url)
         self._major_holders = holders[0]
         self._institutional_holders = holders[1]
-        self._institutional_holders['Date Reported'] = _pd.to_datetime(
-            self._institutional_holders['Date Reported'])
-        self._institutional_holders['% Out'] = self._institutional_holders[
-            '% Out'].str.replace('%', '').astype(float)/100
+        if 'Date Reported' in self._institutional_holders:
+            self._institutional_holders['Date Reported'] = _pd.to_datetime(
+                self._institutional_holders['Date Reported'])
+        if '% Out' in self._institutional_holders:
+            self._institutional_holders['% Out'] = self._institutional_holders[
+                '% Out'].str.replace('%', '').astype(float)/100
 
         # sustainability
         d = {}
