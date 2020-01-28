7385f6a95b8ca9f2d3344c250fa6f532ef5ad15f
jiexiang <jyong@geckylabs.com>
2020-01-28 23:26:19 +0800

    fix error for holders when data is returned partially or not returned at all

--- yfinance/base.py
+++ yfinance/base.py
@@ -282,8 +282,10 @@ class TickerBase():
         # holders
         url = "{}/{}/holders".format(self._scrape_url, self.ticker)
         holders = _pd.read_html(url)
-        self._major_holders = holders[0]
-        self._institutional_holders = holders[1]
+        if len(holders):
+            self._major_holders = holders[0]
+        if len(holders) == 2:
+            self._institutional_holders = holders[1]
         if 'Date Reported' in self._institutional_holders:
             self._institutional_holders['Date Reported'] = _pd.to_datetime(
                 self._institutional_holders['Date Reported'])
