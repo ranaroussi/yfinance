5d92c4a8d6f52c88f84b5a73892010179022572a
jackmoody11 <jackmoody@unc.edu>
2020-03-04 14:12:09 -0500

    CLN: Remove duplicate property

--- yfinance/ticker.py
+++ yfinance/ticker.py
@@ -122,10 +122,6 @@ class Ticker(TickerBase):
         return self.get_dividends()
 
     @property
-    def dividends(self):
-        return self.get_dividends()
-
-    @property
     def splits(self):
         return self.get_splits()
 
