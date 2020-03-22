25c45ebe83e90014f8df26926f696ea718a9f56d
Jay Song <jay@somewhere.local>
2020-03-22 16:18:15 -0400

    Update hour interval to use set instead of array. https://github.com/ranaroussi/yfinance/pull/176

--- yfinance/base.py
+++ yfinance/base.py
@@ -224,7 +224,7 @@ class TickerBase():
         df.index = df.index.tz_localize("UTC").tz_convert(
             data["chart"]["result"][0]["meta"]["exchangeTimezoneName"])
 
-        if params["interval"][-1] in ["m", "h"]:
+        if params["interval"][-1] in {"m", "h"}:
             df.index.name = "Datetime"
         else:
             df.index = _pd.to_datetime(df.index.date)
