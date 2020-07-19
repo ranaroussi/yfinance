a14043e65d627a9516ad0140eb02e4ce933b8a4a
hashbash <aqec@ya.ru>
2020-07-19 19:04:55 +0300

    Update base.py

--- yfinance/base.py
+++ yfinance/base.py
@@ -283,7 +283,10 @@ class TickerBase():
         url = "{}/{}/holders".format(self._scrape_url, self.ticker)
         holders = _pd.read_html(url)
         self._major_holders = holders[0]
-        self._institutional_holders = holders[1]
+        try:
+            self._institutional_holders = holders[1]
+        except IndexError:
+            self._institutional_holders = {}
         if 'Date Reported' in self._institutional_holders:
             self._institutional_holders['Date Reported'] = _pd.to_datetime(
                 self._institutional_holders['Date Reported'])
