f7ce2630bc548f31e3d23e39dcb9938c6fb68f8a
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-03-22 14:58:32 -0600

    added shell class for testing data values

---
+++ test_earnings.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestDataValues(unittest.TestCase):
+    def test_incomplete_data(self):
+        pass
+    def test_no_quarterly_data(self):
+        pass
+    def test_missing_quarter(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main
