4281fd6dbb3509b7338fd0afbea3634d510bcb2a
cameronridderikhoff <cameronridderikhoff@gmail.com>
2021-03-23 12:36:08 -0600

    merging with master

--- test/test_earnings.py
+++ test/test_earnings.py
@@ -35,6 +35,13 @@ class TestEarnings(unittest.TestCase):
 
     self.assertEqual(len(mock_get_json.call_args_list), 2)
 
-
+class TestDataValues(unittest.TestCase):
+    def test_incomplete_data(self):
+        pass
+    def test_no_quarterly_data(self):
+        pass
+    def test_missing_quarter(self):
+        pass
+      
 if __name__ == '__main__':
   unittest.main()
