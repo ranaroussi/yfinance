61469aa56653eac085d575a50eddbb55e1826034
Ran Aroussi <ranaroussi@users.noreply.github.com>
2021-03-24 11:21:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,6 +46,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     platforms=['any'],
     keywords='pandas, yahoo finance, pandas datareader',
