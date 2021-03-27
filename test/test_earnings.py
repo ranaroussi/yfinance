ca75e4572fde1243574a7f3245ce9e888ab954e1
Bowler1 <bowler@ualberta.ca>
2021-03-27 13:09:24 -0600

    Added some tests for invalid tickers

--- test/test_earnings.py
+++ test/test_earnings.py
@@ -11,6 +11,8 @@ import yfinance as yf
 from unittest import mock
 from pathlib import Path
 
+from http.client import InvalidURL
+from urllib.error import HTTPError
 from mock import get_mocked_get_json
 
 # Mock based on https://stackoverflow.com/a/28507806/3558475:
@@ -42,10 +44,28 @@ class TestEarnings(unittest.TestCase):
 
   def test_invalid_ticker(self):
     invalid = yf.Ticker('InvalidTickerName')
-
     with self.assertRaises(ValueError):
         invalid.earnings
 
+    invalid = yf.Ticker('') #test empty
+    with self.assertRaises(HTTPError):
+        invalid.earnings
+
+    invalid = yf.Ticker('LEHLQ') #test debunct
+    with self.assertRaises(KeyError):
+        invalid.earnings
+
+    invalid = yf.Ticker('GOOE') #test misspelled (like GOOE when we want GOOG)
+    with self.assertRaises(KeyError):
+        invalid.earnings
+
+    invalid = yf.Ticker(' ') # test white space
+    with self.assertRaises(InvalidURL):
+        invalid.earnings
+
+    invalid = yf.Ticker('123') # test with numbers
+    with self.assertRaises(KeyError):
+        invalid.earnings
 
 
 if __name__ == '__main__':
