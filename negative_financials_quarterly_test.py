99dea023d4bdbf3700106316202f6c58656c1072
Haochen Gou <32112948+HaochenGou@users.noreply.github.com>
2021-03-27 13:30:53 -0600

    Update negative_financials_quarterly_test.py

--- negative_financials_quarterly_test.py
+++ negative_financials_quarterly_test.py
@@ -1,26 +1,21 @@
-import yfinance as yf
-from unittest import TestCase
+import unittest
+from unittest.mock import Mock
 from unittest.mock import patch
-import pandas as _pd
-import numpy as _np
- 
- 
-class FinancialsTestCase(TestCase):
-  def setUp(self):
-    with patch('yf.Ticker(' ')') as MockTicker:
-    self.ticker = MockTicker()
-    index =[]
-    data = _pd.DataFrame(index =index,data={
-    'Open': _np.nan, 'High': _np.nan, 'Low': _np.nan,
-    'Close': _np.nan, 'Adj Close': _np.nan, 'Volume': _np.nan})
-    ticker.quarterly_financials.return_value = data
-    self.response = ticker.quarterly_financials
-  def test_get_financials_quarterly_invalid_data_returns_empty_dataframe(self):
-    self.assertIsNone(self.response)
-            
-    
-            
-            
-            
+import yfinance as yf
+
+
+class FinancialsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.msft = yf.Ticker("MSFT")
+        self.utils_mock = patch('yfinance.base.utils.get_json', autospec=True).start()
+        self.addCleanup(patch.stopall)
+      
+    def test_get_financials_quarterly_invalid_data_returns_empty_dataframe(self):
+        try:
+            financials = self.msft.quarterly_financials
+            self.assertTrue(financials.empty)
+        except:
+            self.assertTrue(False)
+        
 if __name__ == "__main__":
     unittest.main()
