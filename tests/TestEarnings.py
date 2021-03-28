d4aa6c21013d8f85e99403b09648381172a93c1c
jttbakker <76896826+jttbakker@users.noreply.github.com>
2021-03-28 05:49:33 -0600

    Document test methods

--- tests/TestEarnings.py
+++ tests/TestEarnings.py
@@ -12,15 +12,33 @@ import yfinance.base as yfBase
 
 class TestEarnings(unittest.TestCase):
     def mock_get_json(self, earnings_data):
+        """
+        Returns the sum of two decimal numbers in binary digits.
+
+                Parameters:
+                        earnings_data (dict): Earnings dictionary with a financialChart containing yearly/quarterly earnings
+
+        """
+        
         first_return = {"financialData":{"regularMarketPrice":100, "regularMarketOpen":100}}
         second_return = earnings_data
         yfBase.utils.get_json = Mock(side_effect=[first_return, second_return])
 
     def set_up(self):
+        """
+        Create a TickerBase instance and mock methods that prevent exceptions
+        when _get_fundamentals() is called.
+        """
+        
         self.tb = yfBase.TickerBase("stub_ticker_base")
         yfBase._pd.read_html = MagicMock(return_value=[None,])
 
     def test_yearly_earnings(self):
+        """
+        Test that TickerBase saves yearly earnings using data
+        obtained from a request to ticker_url/financials.
+        """
+        
         self.set_up()
         
         earnings_data = {"earnings":{
@@ -46,6 +64,11 @@ class TestEarnings(unittest.TestCase):
                          self.tb._earnings["yearly"].sort_index(inplace=True))
 
     def test_quarterly_earnings(self):
+        """
+        Test that TickerBase saves quarterly earnings using data
+        obtained from a request to ticker_url/financials.
+        """
+        
         self.set_up()
         
         earnings_data = {"earnings":{
@@ -72,6 +95,10 @@ class TestEarnings(unittest.TestCase):
         
 
     def test_yearly_and_quarterly_earnings(self):
+        """
+        Test that TickerBase saves both yearly and quarterly earnings using data
+        obtained from a request to ticker_url/financials.
+        """
         self.set_up()
         
         earnings_data = {"earnings":{
@@ -93,7 +120,6 @@ class TestEarnings(unittest.TestCase):
         financials_chart = earnings_data["earnings"]["financialsChart"]
         expected_yearly_frame = pandas.DataFrame(financials_chart["yearly"])
         expected_quarterly_frame = pandas.DataFrame(financials_chart["quarterly"])
-        expected_yearly_frame = pandas.DataFrame(financials_chart["yearly"])
         
         self.assertEqual(expected_quarterly_frame.sort_index(inplace=True), 
                          self.tb._earnings["quarterly"].sort_index(inplace=True))
