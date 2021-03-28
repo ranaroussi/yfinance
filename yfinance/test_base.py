c65b742eee33474b94baf7ea7e6e055cfe5e1306
walkP <wpeters@ualberta.ca>
2021-03-27 20:14:40 -0600

    testing stuff

--- yfinance/test_base.py
+++ yfinance/test_base.py
@@ -5,11 +5,11 @@ import base
 class TestGenericPatterns(unittest.TestCase):
     
     def test_test(self):
-        tickerBase = base.TickerBase(ticker="MSFT")
+        msft = base.TickerBase(ticker="MSFT")
         empty_dict = dict()
-        data = tickerBase.generic_patterns(empty_dict)
+        data = msft.generic_patterns(empty_dict)
         self.assertTrue(isinstance(data, dict))
-        self.assertEquals(tickerBase.get_info()['zip'], '98052-6399')
+        self.assertEquals(msft.get_info()['zip'], '98052-6399')
 
 
 if __name__ == '__main__':
