a28ec9d39d8927a76c398809ae1c4cac7743b38e
jttbakker <76896826+jttbakker@users.noreply.github.com>
2021-03-28 05:50:51 -0600

    Refactor imports

--- tests/TestEarnings.py
+++ tests/TestEarnings.py
@@ -1,5 +1,7 @@
 # Get parent directory to import the yfinancial library
-import os,sys,inspect
+import os
+import sys
+import inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
