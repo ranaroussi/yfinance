6d3fa62963aaf081e53b69b5c8a068deea382223
jttbakker <76896826+jttbakker@users.noreply.github.com>
2021-03-28 05:27:24 -0600

    Finish isolation and add 3 basic tests

--- tests/TestEarnings.py
+++ tests/TestEarnings.py
@@ -5,43 +5,103 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
 
 import unittest
-import json
+import pandas
 from unittest.mock import MagicMock, Mock
 import yfinance.base as yfBase
 
 
 class TestEarnings(unittest.TestCase):
-    def get_json_side_effect(*args, **kwargs):
-        if (not args[0].endswith("/financials")):
-            # Return data for the first get_json call
-            data = '''{"banana":"Funny"}'''
-            print(type(json.dumps(data)))
-            return json.loads(data)
-        else:
-            # Return data for the second get_json call
-            data = '''{"banana":"Funny"}'''
-            print(type(json.dumps(data)))
-            return json.loads(data)
-
+    def set_get_json_mock(self, earnings_data):
+        first_return = {"financialData":{"regularMarketPrice":100, "regularMarketOpen":100}}
+        second_return = earnings_data
+        yfBase.utils.get_json = Mock(side_effect=[first_return, second_return])
 
     def set_up(self):
         self.tb = yfBase.TickerBase("stub_ticker_base")
-        self.tb._info = {'regularMarketPrice':100.0, 'regularMarketOpen':100.0}
-        self.data = None
-        yfBase.utils.get_json = Mock(side_effect=TestEarnings.get_json_side_effect)
         yfBase._pd.read_html = MagicMock(return_value=[None,])
+
+    def mock_get_json(self, earnings_data):
+        yfBase.utils.get_json = Mock(return_value={}, side_effect=TestEarnings.create_side_effect(earnings_data))
 
     def test_yearly_earnings(self):
         self.set_up()
+        
+        earnings_data = {"earnings":{
+                                "financialsChart":{
+                                    "yearly":{
+                                        "date":["December 1, 2021"],
+                                        "total":[100]
+                                    },
+                                    "quarterly":{
+                                        "date":["November 2, 2021"],
+                                        "total":[500]
+                                    }
+                                }
+                            }
+                        }
+        self.set_get_json_mock(earnings_data)
         self.tb._get_fundamentals()
+
+        financials_chart = earnings_data["earnings"]["financialsChart"]
+        expected_yearly_frame = pandas.DataFrame(financials_chart["yearly"])
+        
+        self.assertEqual(expected_yearly_frame.sort_index(inplace=True), 
+                         self.tb._earnings["yearly"].sort_index(inplace=True))
 
     def test_quarterly_earnings(self):
         self.set_up()
-        pass
+        
+        earnings_data = {"earnings":{
+                                "financialsChart":{
+                                    "yearly":{
+                                        "date":["December 1, 2021"],
+                                        "total":[100]
+                                    },
+                                    "quarterly":{
+                                        "date":["November 2, 2021"],
+                                        "total":[500]
+                                    }
+                                }
+                            }
+                        }
+        self.set_get_json_mock(earnings_data)
+        self.tb._get_fundamentals()
+        
+        financials_chart = earnings_data["earnings"]["financialsChart"]
+        expected_quarterly_frame = pandas.DataFrame(financials_chart["quarterly"])
+        
+        self.assertEqual(expected_quarterly_frame.sort_index(inplace=True), 
+                         self.tb._earnings["quarterly"].sort_index(inplace=True))
+        
 
     def test_yearly_and_quarterly_earnings(self):
         self.set_up()
-        pass
+        
+        earnings_data = {"earnings":{
+                                "financialsChart":{
+                                    "yearly":{
+                                        "date":["December 1, 2021"],
+                                        "total":[100]
+                                    },
+                                    "quarterly":{
+                                        "date":["November 2, 2021"],
+                                        "total":[500]
+                                    }
+                                }
+                            }
+                        }
+        self.set_get_json_mock(earnings_data)
+        self.tb._get_fundamentals()
+
+        financials_chart = earnings_data["earnings"]["financialsChart"]
+        expected_yearly_frame = pandas.DataFrame(financials_chart["yearly"])
+        expected_quarterly_frame = pandas.DataFrame(financials_chart["quarterly"])
+        expected_yearly_frame = pandas.DataFrame(financials_chart["yearly"])
+        
+        self.assertEqual(expected_quarterly_frame.sort_index(inplace=True), 
+                         self.tb._earnings["quarterly"].sort_index(inplace=True))
+        self.assertEqual(expected_yearly_frame.sort_index(inplace=True), 
+                         self.tb._earnings["yearly"].sort_index(inplace=True))
 
 if __name__ == '__main__':
     unittest.main()
