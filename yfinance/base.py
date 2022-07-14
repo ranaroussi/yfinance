c162af84b2a9d392d8691e34204278685658bb87
Value Raider <ValueRaider@protonmail.com>
2022-07-14 23:04:41 +0100

    Relocate out-of-range check to before 15m->30m interpolation

--- yfinance/base.py
+++ yfinance/base.py
@@ -239,6 +239,11 @@ class TickerBase():
         # parse quotes
         try:
             quotes = utils.parse_quotes(data["chart"]["result"][0], tz)
+            # Yahoo bug fix - it often appends latest price even if after end date
+            if end and not quotes.empty:
+                endDt = _pd.to_datetime(_datetime.datetime.fromtimestamp(end))
+                if quotes.index[quotes.shape[0]-1] > endDt:
+                    quotes = quotes.iloc[0:quotes.shape[0]-1]
         except Exception:
             shared._DFS[self.ticker] = utils.empty_df()
             shared._ERRORS[self.ticker] = err_msg
@@ -290,12 +295,6 @@ class TickerBase():
 
         # actions
         dividends, splits = utils.parse_actions(data["chart"]["result"][0], tz)
-
-        # Yahoo bug fix - it often appends latest price even if after end date
-        if end and not quotes.empty:
-            endDt = _pd.to_datetime(_datetime.datetime.fromtimestamp(end))
-            if quotes.index[quotes.shape[0]-1] > endDt:
-                quotes = quotes.iloc[0:quotes.shape[0]-1]
 
         # combine
         df = _pd.concat([quotes, dividends, splits], axis=1, sort=True)
