81045865e321e478608bfb8c33a1257794b711c6
ValueRaider <ValueRaider@protonmail.com>
2022-10-30 18:15:43 +0000

    Add date args to 'shares_full()' and caching

--- yfinance/ticker.py
+++ yfinance/ticker.py
@@ -132,9 +132,9 @@ class Ticker(TickerBase):
     @property
     def shares(self) -> _pd.DataFrame :
         return self.get_shares()
-    @property
-    def shares_full(self):
-        return self.get_shares_full()
+    # @property
+    def shares_full(self, start=None, end=None):
+        return self.get_shares_full(start, end)
 
     @property
     def info(self) -> dict:
