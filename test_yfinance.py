d261237320eb2824cc86e77c294692a17ac28f03
ValueRaider <ValueRaider@protonmail.com>
2022-11-03 22:49:12 +0000

    Improve handling delisted tickers

--- test_yfinance.py
+++ test_yfinance.py
@@ -15,24 +15,82 @@ Sanity check for most common library uses all working
 
 import yfinance as yf
 import unittest
+import datetime
+
+session = None
+import requests_cache ; session = requests_cache.CachedSession("yfinance.cache", expire_after=24*60*60)
 
 symbols = ['MSFT', 'IWO', 'VFINX', '^GSPC', 'BTC-USD']
-tickers = [yf.Ticker(symbol) for symbol in symbols]
+tickers = [yf.Ticker(symbol, session=session) for symbol in symbols]
+delisted_symbols = ["BRK.B", "SDLP"]
+delisted_tickers = [yf.Ticker(symbol, session=session) for symbol in delisted_symbols]
 
 
 class TestTicker(unittest.TestCase):
-    def test_info_history(self):
-        for ticker in tickers:
-            # always should have info and history for valid symbols
-            assert(ticker.info is not None and ticker.info != {})
-            history = ticker.history(period="max")
-            assert(history.empty is False and history is not None)
+    def setUp(self):
+        d_today = datetime.date.today()
+        d_today -= datetime.timedelta(days=30)
+        self.start_d = datetime.date(d_today.year, d_today.month, 1)
 
-        histories = yf.download(symbols, period="1yr")
+    def test_info_history(self):
+        # always should have info and history for valid symbols
+        for ticker in tickers:
+            assert(ticker.info is not None and ticker.info != {})
+            history = ticker.history(period="1mo")
+            assert(history.empty is False and history is not None)
+        histories = yf.download(symbols, period="1mo", session=session)
         assert(histories.empty is False and histories is not None)
+
+        for ticker in tickers:
+            assert(ticker.info is not None and ticker.info != {})
+            history = ticker.history(start=self.start_d)
+            assert(history.empty is False and history is not None)
+        histories = yf.download(symbols, start=self.start_d, session=session)
+        assert(histories.empty is False and histories is not None)
+
+    def test_info_history_nofail(self):
+        # should not throw Exception for delisted tickers, just print a message
+        for ticker in delisted_tickers:
+            history = ticker.history(period="1mo")
+        histories = yf.download(delisted_symbols, period="1mo", session=session)
+        histories = yf.download(delisted_symbols[0], period="1mo", session=session)
+        histories = yf.download(delisted_symbols[1], period="1mo")#, session=session)
+        for ticker in delisted_tickers:
+            history = ticker.history(start=self.start_d)
+        histories = yf.download(delisted_symbols, start=self.start_d, session=session)
+        histories = yf.download(delisted_symbols[0], start=self.start_d, session=session)
+        histories = yf.download(delisted_symbols[1], start=self.start_d, session=session)
 
     def test_attributes(self):
         for ticker in tickers:
+            ticker.isin
+            ticker.major_holders
+            ticker.institutional_holders
+            ticker.mutualfund_holders
+            ticker.dividends
+            ticker.splits
+            ticker.actions
+            ticker.info
+            ticker.calendar
+            ticker.recommendations
+            ticker.earnings
+            ticker.quarterly_earnings
+            ticker.financials
+            ticker.quarterly_financials
+            ticker.balance_sheet
+            ticker.quarterly_balance_sheet
+            ticker.cashflow
+            ticker.quarterly_cashflow
+            ticker.sustainability
+            ticker.options
+            ticker.news
+            ticker.shares
+            ticker.earnings_history
+            ticker.earnings_dates
+
+    def test_attributes_nofail(self):
+        # should not throw Exception for delisted tickers, just print a message
+        for ticker in delisted_tickers:
             ticker.isin
             ticker.major_holders
             ticker.institutional_holders
