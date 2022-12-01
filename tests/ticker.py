4e04f367dab771724a2b44dbf23c1745216b710b
ValueRaider <ValueRaider@protonmail.com>
2022-12-01 15:47:37 +0000

    Fix dev merge ; Fix financials fallback fetch

--- tests/ticker.py
+++ tests/ticker.py
@@ -246,9 +246,24 @@ class TestTickerHolders(unittest.TestCase):
 
 
 class TestTickerMiscFinancials(unittest.TestCase):
+    session = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.session = requests_cache.CachedSession(backend='memory')
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.session is not None:
+            cls.session.close()
 
     def setUp(self):
-        self.ticker = yf.Ticker("GOOGL")
+        self.ticker = yf.Ticker("GOOGL", session=self.session)
+
+        # For ticker 'BSE.AX' (and others), Yahoo not returning 
+        # full quarterly financials (usually cash-flow) with all entries, 
+        # instead returns a smaller version in different data store.
+        self.ticker_old_fmt = yf.Ticker("BSE.AX", session=self.session)
 
     def tearDown(self):
         self.ticker = None
@@ -281,6 +296,16 @@ class TestTickerMiscFinancials(unittest.TestCase):
         for k in expected_keys:
             self.assertIn(k, data.index, "Did not find expected row in index")
 
+    def test_quarterly_income_statement_old_fmt(self):
+        expected_row = "TotalRevenue"
+        data = self.ticker_old_fmt.quarterly_income_stmt
+        self.assertIsInstance(data, pd.DataFrame, "data has wrong type")
+        self.assertFalse(data.empty, "data is empty")
+        self.assertIn(expected_row, data.index, "Did not find expected row in index")
+
+        data_cached = self.ticker_old_fmt.quarterly_income_stmt
+        self.assertIs(data, data_cached, "data not cached")
+
     def test_balance_sheet(self):
         expected_row = "TotalAssets"
         data = self.ticker.balance_sheet
@@ -299,6 +324,16 @@ class TestTickerMiscFinancials(unittest.TestCase):
         self.assertIn(expected_row, data.index, "Did not find expected row in index")
 
         data_cached = self.ticker.quarterly_balance_sheet
+        self.assertIs(data, data_cached, "data not cached")
+
+    def test_quarterly_balance_sheet_old_fmt(self):
+        expected_row = "TotalAssets"
+        data = self.ticker_old_fmt.quarterly_balance_sheet
+        self.assertIsInstance(data, pd.DataFrame, "data has wrong type")
+        self.assertFalse(data.empty, "data is empty")
+        self.assertIn(expected_row, data.index, "Did not find expected row in index")
+
+        data_cached = self.ticker_old_fmt.quarterly_balance_sheet
         self.assertIs(data, data_cached, "data not cached")
 
     def test_balance_sheet_formatting(self):
@@ -327,6 +362,16 @@ class TestTickerMiscFinancials(unittest.TestCase):
         self.assertIn(expected_row, data.index, "Did not find expected row in index")
 
         data_cached = self.ticker.quarterly_cashflow
+        self.assertIs(data, data_cached, "data not cached")
+
+    def test_quarterly_cashflow_old_fmt(self):
+        expected_row = "NetIncome"
+        data = self.ticker_old_fmt.quarterly_cashflow
+        self.assertIsInstance(data, pd.DataFrame, "data has wrong type")
+        self.assertFalse(data.empty, "data is empty")
+        self.assertIn(expected_row, data.index, "Did not find expected row in index")
+
+        data_cached = self.ticker_old_fmt.quarterly_cashflow
         self.assertIs(data, data_cached, "data not cached")
 
     def test_cashflow_formatting(self):
