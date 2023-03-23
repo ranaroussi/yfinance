efd12ff9135e6d8cecb00aafb29d2cd593aa9a45
BookSword <qianyun210603@hotmail.com>
2023-03-23 10:08:16 +0800

    Fetch 'info' dict via API

--- yfinance/data.py
+++ yfinance/data.py
@@ -15,6 +15,8 @@ else:
 import requests as requests
 import re
 from bs4 import BeautifulSoup
+import random
+import time
 
 from frozendict import frozendict
 
@@ -201,6 +203,11 @@ class TickerData:
                 proxy = proxy["https"]
             proxy = {"https": proxy}
         return proxy
+
+    def get_raw_json(self, url, user_agent_headers=None, params=None, proxy=None, timeout=30):
+        response = self.get(url, user_agent_headers=user_agent_headers, params=params, proxy=proxy, timeout=timeout)
+        response.raise_for_status()
+        return response.json()
 
     def _get_decryption_keys_from_yahoo_js(self, soup):
         result = None
