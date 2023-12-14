39fe4b25b6e8e15b418052a415fd564d96180e76
Unit <admin@ghofi.net>
2023-12-14 22:06:00 +0100

    rewriting dataframe creation

--- yfinance/scrapers/quote.py
+++ yfinance/scrapers/quote.py
@@ -594,25 +594,7 @@ class Quote:
     @property
     def calendar(self) -> pd.DataFrame:
         if self._calendar is None:
-            result = self._fetch(self.proxy, modules=['calendarEvents'])
-            try:
-                fileds = ['exDividendDate', 'dividendDate']
-                result = result["quoteSummary"]["result"][0]["calendarEvents"]
-                data = dict()
-                for field in fileds:
-                    if field in result:
-                        data[field] = result[field]
-                earnings = result.get('earnings')
-                if earnings is not None and len(earnings.get('earningsDate', [])) > 0:
-                    data['earningsDate'] = earnings['earningsDate'][0]
-                df = pd.DataFrame.from_dict(data, orient='index')
-                if not df.empty:
-                    df.columns = ['Date']
-                    df.index.name = 'Event'
-                    df['Date'] = pd.to_datetime(df['Date'], unit='s').dt.date
-                self._calendar = df
-            except (KeyError, IndexError):
-                raise YFinanceDataException(f"Failed to parse json response from Yahoo Finance: {result}")
+            self._fecth_calendar()
         return self._calendar
 
     @staticmethod
@@ -729,3 +711,41 @@ class Quote:
             except Exception:
                 v = None
             self._info[k] = v
+
+    def _fecth_calendar(self):
+        # secFilings return too old data, so not requesting it for now
+        result = self._fetch(self.proxy, modules=['calendarEvents'])
+        try:
+            dates = []
+            events = []
+            earnings_stats = None
+            _events = result["quoteSummary"]["result"][0]["calendarEvents"]
+            if 'dividendDate' in _events:
+                dates.append(str(datetime.datetime.fromtimestamp(_events['dividendDate']).date()))
+                events.append('Dividend Date')
+            if 'exDividendDate' in _events:
+                dates.append(str(datetime.datetime.fromtimestamp(_events['exDividendDate']).date()))
+                events.append('Ex-Dividend Date')
+            # splits = _events.get('splitDate')  # need to check later, i will add code for this if found data
+            earnings = _events.get('earnings')
+            if earnings is not None:
+                _dates = earnings.get('earningsDate', [])
+                earnings['earningsDate'] = str(datetime.datetime.fromtimestamp(_dates[0]).date()) \
+                    if len(_dates) == 1 \
+                    else str(datetime.datetime.fromtimestamp(_dates[0]).date()) + ' - ' + str(datetime.datetime.fromtimestamp(_dates[1]).date())
+                dates.append(earnings['earningsDate'])
+                events.append('Earnings Date')
+                earnings_stats = pd.DataFrame.from_dict(earnings, orient='index')
+                earnings_stats.columns = ['Value']
+                earnings_stats.index.name = 'Key'
+            # return empty dataframe if no events
+            if len(dates) == 0:
+                self._calendar = pd.DataFrame()
+                return
+            # create calendar dataframe
+            df = pd.DataFrame({'Event': events, 'Date': dates})
+            # add earnings stats as attributes
+            df.attrs['earnings'] = earnings_stats
+            self._calendar = df
+        except (KeyError, IndexError):
+            raise YFinanceDataException(f"Failed to parse json response from Yahoo Finance: {result}")
