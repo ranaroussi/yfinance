76062dda22ba6ca73f361800d0b8d1c758d1d345
Eric Pien <eric.hs.pien@gmail.com>
2024-08-10 00:29:07 -0700

    fixing minor typo
    %% -> %

--- yfinance/utils.py
+++ yfinance/utils.py
@@ -885,7 +885,7 @@ class ProgressBar:
         num_hashes = int(round((percent_done / 100.0) * all_full))
         self.prog_bar = '[' + self.fill_char * num_hashes + ' ' * (all_full - num_hashes) + ']'
         pct_place = (len(self.prog_bar) // 2) - len(str(percent_done))
-        pct_string = f'{percent_done}%%'
+        pct_string = f'{percent_done}%'
         self.prog_bar = self.prog_bar[0:pct_place] + (pct_string + self.prog_bar[pct_place + len(pct_string):])
 
     def __str__(self):
