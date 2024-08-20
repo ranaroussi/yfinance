683bb0dd07eb5ddbd327d7ca053f8262727b9dcc
Aleks Fasting <aleks.fasting@gmail.com>
2024-08-20 02:57:14 +0200

    changed metadata that time is fetched from

--- yfinance/scrapers/history.py
+++ yfinance/scrapers/history.py
@@ -227,13 +227,8 @@ class PriceHistory:
         # 2) fix weired bug with Yahoo! - returning 60m for 30m bars
         if interval.lower() == "30m":
             logger.debug(f'{self.ticker}: resampling 30m OHLC from 15m')
-            try:
-                exchangeStartTime = pd.Timestamp(self._history_metadata["tradingPeriods"][0][0]["start"], unit='s')
-                offset = str(exchangeStartTime.minute % 30)+"min"
-            except KeyError:
-                offset = '15min'
-            except IndexError:
-                offset = '15min'
+            exchangeStartTime = pd.Timestamp(self._history_metadata["currentTradingPeriod"]['regular']["start"], unit='s')
+            offset = str(exchangeStartTime.minute % 30)+"min"
             quotes2 = quotes.resample('30min', offset=offset)
             quotes = pd.DataFrame(index=quotes2.last().index, data={
                 'Open': quotes2['Open'].first(),
