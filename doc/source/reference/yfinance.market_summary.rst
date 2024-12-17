f2769a23162b52c97a577ed6717f29b972ef19b8
R5dan <r5dan@outlook.com>
2024-12-17 20:28:48 +0000

    Removed unpacking of the dictionary of results

--- doc/source/reference/yfinance.market_summary.rst
+++ doc/source/reference/yfinance.market_summary.rst
@@ -4,7 +4,7 @@ Market Summary
 .. currentmodule:: yfinance
 Class
 ------------
-The `MarketSummary` class, allows you to access market summary data in a Pythonic way.
+The `Summary` class, allows you to access market summary data in a Pythonic way.
 .. autosummary::
    :toctree: api/
    Summary
@@ -14,10 +14,3 @@ Summary Sample Code
 The `Summary` class, allows you to access market summary data in a Pythonic way.
 .. literalinclude:: examples/market_summary.py
    :language: python
-
-Class
-------------
-The `MarketSummary` class, allows you to access infomation about a specific market.
-.. autosumary::
-   :toctree: api/
-   MarketSummary
--- yfinance/summary.py
+++ yfinance/summary.py
@@ -5,55 +5,11 @@ from . import utils
 from .const import _QUERY1_URL_
 from .data import YfData
 
-class MarketSummary:
-    def __init__(self, data) -> None:
-        # Basic information
-        self.name: 'str' = data.get("shortName")
-        self.region: 'str' = data.get("region")
-        self.market: 'str' = data.get("market")
-        self.quote_type: 'str' = data.get("quoteType")
-        self.type_display: 'str' = data.get("typeDisp")
-        self.symbol: 'str' = data.get("symbol")
-        
-        # Market data
-        self.price: 'float' = data.get("regularMarketPrice")
-        self.change: 'float' = data.get("regularMarketChange")
-        self.change_percent: 'float' = data.get("regularMarketChangePercent")
-        self.previous_close: 'float' = data.get("regularMarketPreviousClose")
-        self.market_time: 'int' = data.get("regularMarketTime")
-        self.market_state: 'str' = data.get("marketState")
-        self.price_hint: 'int' = data.get("priceHint")
-        
-        # Exchange information
-        self.exchange: 'str' = data.get("exchange")
-        self.full_exchange_name: 'str' = data.get("fullExchangeName")
-        self.timezone: 'str' = data.get("exchangeTimezoneName")
-        self.timezone_short: 'str' = data.get("exchangeTimezoneShortName")
-        self.gmt_offset: 'int' = data.get("gmtOffSetMilliseconds")
-        self.exchange_delay: 'int' = data.get("exchangeDataDelayedBy")
-        
-        # Quote information
-        self.quote_source: 'str' = data.get("quoteSourceName")
-        self.source_interval: 'int' = data.get("sourceInterval")
-        
-        # Trading properties
-        self.triggerable: 'bool' = data.get("triggerable")
-        self.tradeable: 'bool' = data.get("tradeable")
-        self.crypto_tradeable: 'bool' = data.get("cryptoTradeable")
-        self.has_pre_post_market: 'bool' = data.get("hasPrePostMarketData")
-        self.first_trade_date: 'int' = data.get("firstTradeDateMilliseconds")
-        
-        # Additional properties
-        self.esg_populated: 'bool' = data.get("esgPopulated")
-        self.price_alert_confidence: 'str' = data.get("customPriceAlertConfidence")
-
-
-
 class Summary:
     def __init__(self, market, region="US", session=None, proxy=None, timeout=30, raise_errors=True, refresh=3600):
         """
         market: The market area you want to get the summary for.
-        region: Can only be US
+        region: Used for yahoo logging purposes.
         refresh: The number of seconds to wait before refreshing the data.
         """
         self.market = market
@@ -69,7 +25,6 @@ class Summary:
         self._data = YfData(session=session)
         self._logger = utils.get_yf_logger()
         self._last_refresh = 0
-        self._last_data = []
 
     def _request(self, formatted=False, fields=["shortName", "regularMarketPrice", "regularMarketChange", "regularMarketChangePercent"]):
         url = f"{_QUERY1_URL_}/v6/finance/quote/marketSummary"
@@ -101,16 +56,17 @@ class Summary:
             # As the data is very unlikely to change, we can cache it for a long time.
             self._last_refresh = time.time()
             self._last_request = self._request(formatted=formatted, fields=fields)["marketSummaryResponse"]
-            self._convert_data()
         
         return self._last_request
-    
-    def _convert_data(self):
-        self._last_data = [MarketSummary(data) for data in self._last_request["result"]]
 
-    def __getitem__(self, name: str) -> 'MarketSummary':
+    def __getitem__(self, name: str) -> 'dict':
         self.refresh_data()
-        for data in self._last_data:
-            if data.symbol == name:
+        for data in self.data:
+            if data["symbol"] == name:
                 return data
-        raise AttributeError(f"{name} is not a symbol in {self.market}. Valid symbols are: {", ".join([data.symbol for data in self._last_data])}")
+        raise AttributeError(f"{name} is not a symbol in {self.market}. Valid symbols are: {", ".join([data["symbol"] for data in self.data])}")
+    
+    @property
+    def data(self) -> 'list[dict]':
+        self.refresh_data()
+        return self._last_request["result"]
