588355d490535a7dad855eb841c3f0a52c0091d1
R5dan <r5dan@outlook.com>
2025-01-30 12:43:31 +0000

    fix

--- yfinance/screener/__init__.py
+++ yfinance/screener/__init__.py
@@ -1,4 +1,3 @@
-from .query import EquityQuery, screen, FundQuery, Query, QueryHead
-from .const import PREDEFINED_SCREENER_QUERIES
+from .query import EquityQuery, screen, FundQuery, Query, QueryHead, PREDEFINED_SCREENER_QUERIES
 
 __all__ = ["EquityQuery", "FundQuery", "screen", "PREDEFINED_SCREENER_QUERIES", "Query", "QueryHead"]
