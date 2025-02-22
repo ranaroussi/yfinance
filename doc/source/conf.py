0ecaf96ccb1018aac1aea6f80c2e7725e5272e98
Ran Aroussi <ranaroussi@users.noreply.github.com>
2025-02-22 22:17:21 +0000

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -10,8 +10,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'yfinance - market data downloader'
-copyright = '2017-2019 Ran Aroussi'
+project = 'yfinance / Pythonic access to market data'
+copyright = '2017-2025 Ran Aroussi'
 author = 'Ran Aroussi'
 
 # -- General configuration ---------------------------------------------------
@@ -41,6 +41,10 @@ html_theme = 'pydata_sphinx_theme'
 html_theme_options = {
     "github_url": "https://github.com/ranaroussi/yfinance",
     "navbar_align": "left"
+    "logo": {
+      "image_light": "_static/logo-light.webp",
+      "image_dark": "_static/logo-dark.webp",
+   }
 }
 html_static_path = ['_static']
 html_css_files = ['yfinance.css']
