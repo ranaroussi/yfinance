f763c44c549f6e39d1255a704b74afa551aa19bf
Dhruvan Gnanadhandayuthapani <d.gnanadhandayuthapani@student.tudelft.nl>
2025-03-16 02:57:38 +0100

    Add unit tests for Lookup functionality

---
+++ tests/test_lookup.py
@@ -0,0 +1,67 @@
+import unittest
+
+from tests.context import yfinance as yf, session_gbl
+
+
+class TestLookup(unittest.TestCase):
+    def setUp(self):
+        self.query = "A"  # Generic query to make sure all lookup types are returned
+        self.lookup = yf.Lookup(query=self.query, session=session_gbl)
+
+    def test_get_all(self):
+        result = self.lookup.get_all(count=5)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+
+    def test_get_equity(self):
+        result = self.lookup.get_equity(count=5)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+
+    def test_get_mutualfund(self):
+        result = self.lookup.get_mutualfund(count=5)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+
+    def test_get_etf(self):
+        result = self.lookup.get_etf(count=5)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+
+    def test_get_index(self):
+        result = self.lookup.get_index(count=5)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+
+    def test_get_future(self):
+        result = self.lookup.get_future(count=5)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+
+    def test_get_currency(self):
+        result = self.lookup.get_currency(count=5)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+
+    def test_get_cryptocurrency(self):
+        result = self.lookup.get_cryptocurrency(count=5)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+
+    def test_large_all(self):
+        result = self.lookup.get_all(count=1000)
+
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 1000)
+
+
+if __name__ == "__main__":
+    unittest.main()
