591c6f562d930fee60be395e5e6d53a433a67070
Gaze31 <velvetgazeze@gmail.com>
2026-04-18 20:15:23 +0530

    restore blank line between try/except and metadata format check

--- yfinance/scrapers/history.py
+++ yfinance/scrapers/history.py
@@ -546,13 +546,18 @@ class PriceHistory:
         return self._history_cache[cache_key]
 
     def get_history_metadata(self) -> dict:
-       if self._history_metadata is None or 'tradingPeriods' not in self._history_metadata:
-        # Request intraday data, because then Yahoo returns exchange schedule (tradingPeriods).
-        try:
-            self._get_history_cache(period="5d", interval="1h")['prices']
-        except Exception:
-            pass  # tradingPeriods is optional enrichment; don't fail if intraday unavailable
+       def get_history_metadata(self) -> dict:
+        if self._history_metadata is None or 'tradingPeriods' not in self._history_metadata:
+            try:
+                self._get_history_cache(period="5d", interval="1h")['prices']
+            except Exception:
+                pass  # tradingPeriods is optional enrichment; don't fail if intraday unavailable
 
+        if self._history_metadata_formatted is False:
+            self._history_metadata = utils.format_history_metadata(self._history_metadata)
+            self._history_metadata_formatted = True
+
+        return self._history_metadata
     def get_dividends(self, period="max", repair=False) -> pd.Series:
         return self._get_history_cache(interval='1d', period=period, repair=repair)['dividends']
 
